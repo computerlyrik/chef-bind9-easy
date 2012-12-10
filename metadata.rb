a828ec0def9d2d9955c09ff2e79f173215227163
Christian Fischer <sw-dev@computerlyrik.de>
2012-12-10 12:23:01 +0100

    set to release version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs and autoconfigures bind9 nameserver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "0.0.1"
