18a32a7a9f6478b61ec86bfd13d59f14c21625a0
Christian Fischer <sw-dev@computerlyrik.de>
2014-10-28 21:25:57 +0100

    bumping to version 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs and autoconfigures bind9 nameserver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.1"
+version          "1.0.0"
 
 supports 'debian'
 supports 'ubuntu'
