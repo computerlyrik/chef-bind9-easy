c5b9e133f0c2fd47c10065b964df1ae4268ab1e6
Israel E. Bethencourt <israel_bethencourt@intuit.com>
2015-04-15 13:02:35 +0100

    automatic package selection

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,11 +3,13 @@ default['bind9-easy']['id'] = {}
 default['bind9-easy']['forward'] = []
 default['bind9-easy']['transfer'] = []
 default['bind9-easy']['trusted_server'] = []
-default['bind9-easy']['package'] = 'bind9'
+
 
 case node["platform"]
 when "debian", "ubuntu"
    default['bind9-easy']['service'] = 'bind9'
+   default['bind9-easy']['package'] = 'bind9'
 when "redhat", "centos", "fedora"
    default['bind9-easy']['service'] = 'named'
+   default['bind9-easy']['package'] = 'bind'
 end
